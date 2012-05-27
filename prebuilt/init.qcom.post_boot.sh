4f7d90765ba31ea3bdb8919711af9ef00d4a1afa
Derek Sevilla <deryk0917@gmail.com>
2012-05-26 20:32:49 -0700

    Added back mpdecision and thermald lines This is a test

--- prebuilt/init.qcom.post_boot.sh
+++ prebuilt/init.qcom.post_boot.sh
@@ -30,7 +30,6 @@ target=`getprop ro.board.platform`
 case "$target" in
     "msm7201a_ffa" | "msm7201a_surf" | "msm7627_ffa" | "msm7627_6x" | "msm7627a"  | "msm7627_surf" | \
     "qsd8250_surf" | "qsd8250_ffa" | "msm7630_surf" | "msm7630_1x" | "msm7630_fusion" | "qsd8650a_st1x")
-        echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
         echo 90 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
         ;;
 esac
@@ -48,14 +47,23 @@ case "$target" in
         ;;
 esac
 
+# Target msm7x30: HTC Runnymede, Bliss, Mecha, Flyer, Ace, Saga, etc.
 case "$target" in
-     "msm7201a_ffa" | "msm7201a_surf" | "msm7627_ffa" | "msm7627_6x" | "msm7627_surf" | "msm7630_surf" | "msm7630_1x" | "msm7630_fusion" | "msm7627a" )
-        echo 245760 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+     "msm7x30")
+        echo 50000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
+        echo 90 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
+        echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
+        echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
+        # /system/lib/libqct-opt JNI native will write mfreq to set CPU freq to max for performance
+        chown root.system /sys/devices/system/cpu/mfreq
+        chmod 220 /sys/devices/system/cpu/mfreq
+        # Allow PowerManagerService to acquire perflock and enable perf mode
+        chown system /sys/power/perflock
         ;;
 esac
 
 case "$target" in
-    "msm8660")
+    "msm8660" | "msm8660_csfb")
 	 echo 1 > /sys/module/rpm_resources/enable_low_power/L2_cache
 	 echo 1 > /sys/module/rpm_resources/enable_low_power/pxo
 	 echo 2 > /sys/module/rpm_resources/enable_low_power/vdd_dig
@@ -81,16 +89,16 @@ case "$target" in
 	 echo 90 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
 	 echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
 	 echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
-	 chown system /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
 	 chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 	 chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 	 chown system /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
 	 chown system /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
-         chown system /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
 	 chown root.system /sys/devices/system/cpu/mfreq
 	 chmod 220 /sys/devices/system/cpu/mfreq
 	 chown root.system /sys/devices/system/cpu/cpu1/online
 	 chmod 664 /sys/devices/system/cpu/cpu1/online
+         # Allow PowerManagerService to acquire perflock and enable perf mode
+         chown system /sys/power/perflock
         ;;
 esac
 
@@ -100,25 +108,30 @@ case "$target" in
      echo 1 > /sys/module/rpm_resources/enable_low_power/pxo
      echo 1 > /sys/module/rpm_resources/enable_low_power/vdd_dig
      echo 1 > /sys/module/rpm_resources/enable_low_power/vdd_mem
-     echo 0 > /sys/module/pm_8x60/modes/cpu0/standalone_power_collapse/suspend_enabled
-     echo 0 > /sys/module/pm_8x60/modes/cpu1/standalone_power_collapse/suspend_enabled
-     echo 0 > /sys/module/pm_8x60/modes/cpu0/standalone_power_collapse/idle_enabled
-     echo 0 > /sys/module/pm_8x60/modes/cpu1/standalone_power_collapse/idle_enabled
      echo 1 > /sys/module/pm_8x60/modes/cpu0/power_collapse/suspend_enabled
      echo 1 > /sys/module/pm_8x60/modes/cpu1/power_collapse/suspend_enabled
+     echo 1 > /sys/module/pm_8x60/modes/cpu0/standalone_power_collapse/suspend_enabled
+     echo 1 > /sys/module/pm_8x60/modes/cpu1/standalone_power_collapse/suspend_enabled
+     echo 1 > /sys/module/pm_8x60/modes/cpu0/standalone_power_collapse/idle_enabled
+     echo 1 > /sys/module/pm_8x60/modes/cpu1/standalone_power_collapse/idle_enabled
      echo 1 > /sys/module/pm_8x60/modes/cpu0/power_collapse/idle_enabled
      echo 1 > /sys/module/pm_8x60/modes/cpu1/power_collapse/idle_enabled
+     echo 1 > /sys/module/pm_8660/modes/cpu0/power_collapse/suspend_enabled
+     echo 1 > /sys/module/pm_8660/modes/cpu1/power_collapse/suspend_enabled
+     echo 1 > /sys/module/pm_8660/modes/cpu0/standalone_power_collapse/suspend_enabled
+     echo 1 > /sys/module/pm_8660/modes/cpu1/standalone_power_collapse/suspend_enabled
+     echo 1 > /sys/module/pm_8660/modes/cpu0/standalone_power_collapse/idle_enabled
+     echo 1 > /sys/module/pm_8660/modes/cpu1/standalone_power_collapse/idle_enabled
+     echo 1 > /sys/module/pm_8660/modes/cpu0/power_collapse/idle_enabled
+     echo 1 > /sys/module/pm_8660/modes/cpu1/power_collapse/idle_enabled
      echo 90 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
      echo 50000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
      echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
      echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
-     echo 10 > /sys/devices/system/cpu/cpufreq/ondemand/down_differential
-     chown system /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
      chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
      chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
      chown system /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
      chown system /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
-     chown system /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
      chown root.system /sys/devices/system/cpu/mfreq
      chmod 220 /sys/devices/system/cpu/mfreq
      chown root.system /sys/devices/system/cpu/cpu1/online
@@ -144,6 +157,8 @@ case "$target" in
     ;;
 esac
 
+chown system /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
+
 emmc_boot=`getprop ro.emmc`
 case "$emmc_boot"
     in "1")
@@ -151,5 +166,19 @@ case "$emmc_boot"
         chown system /sys/devices/platform/rs300000a7.65536/sync_sts
         chown system /sys/devices/platform/rs300100a7.65536/force_sync
         chown system /sys/devices/platform/rs300100a7.65536/sync_sts
+    ;;
+esac
+
+
+# Post-setup services
+case "$target" in
+    "msm8660" | "msm8660_csfb" | "msm8960")
+        start mpdecision
+	;;
+esac
+
+case "$target" in
+    "msm8660" | "msm8660_csfb")
+        start thermald
     ;;
 esac
