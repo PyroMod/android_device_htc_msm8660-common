eafb7b2f98c7eb3c7cbf71edf1a06c0a88de9bd7
Derek Sevilla <deryk0917@gmail.com>
2012-06-27 12:40:43 -0700

    Remove lines that are included in vendor/cm/config/common.mk

--- msm8660.mk
+++ msm8660.mk
@@ -70,12 +70,6 @@ PRODUCT_PACKAGES += \
     com.android.future.usb.accessory \
     Torch
 
-# Live Wallpapers
-PRODUCT_PACKAGES += \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    VisualizationWallpapers
-
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     make_ext4fs \
