e3f4139d9aa1bacffb822bd800f68356d80881a8
Derek Sevilla <deryk0917@gmail.com>
2012-08-01 12:59:22 -0700

    Removed ondemand lines Do not need since other governors may be selected

--- prebuilt/init.qcom.post_boot.sh
+++ prebuilt/init.qcom.post_boot.sh
@@ -50,16 +50,10 @@ case "$target" in
 	 echo 1 > /sys/module/pm_8660/modes/cpu1/power_collapse/idle_enabled
 	 echo 1 > /sys/module/pm_8660/modes/cpu0/standalone_power_collapse/idle_enabled
 	 echo 1 > /sys/module/pm_8660/modes/cpu1/standalone_power_collapse/idle_enabled
-	 echo 50000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
-	 echo 90 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
-	 echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
-	 echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
-	 chown system /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
 	 chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 	 chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 	 chown system /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
 	 chown system /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
-	 chown system /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
 	 chown root.system /sys/devices/system/cpu/mfreq
 	 chmod 220 /sys/devices/system/cpu/mfreq
 	 chown root.system /sys/devices/system/cpu/cpu1/online
@@ -76,3 +70,4 @@ case "$emmc_boot"
         chown system /sys/devices/platform/rs300100a7.65536/sync_sts
     ;;
 esac
+
