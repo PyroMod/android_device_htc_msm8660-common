9ccdafd258cd6987900549072a4b8b00341720f5
Derek Sevilla <deryk0917@gmail.com>
2012-09-15 21:58:39 -0700

    msm8660-common: Added min_free_kbytes

--- prebuilt/init.qcom.post_boot.sh
+++ prebuilt/init.qcom.post_boot.sh
@@ -49,6 +49,7 @@ case "$target" in
 	 chown root.system /sys/devices/system/cpu/cpu1/online
 	 chmod 664 /sys/devices/system/cpu/cpu1/online
          mount -t debugfs none /sys/kernel/debug
+         echo "4096" > /proc/sys/vm/min_free_kbytes
          echo "10" > /proc/sys/vm/swappiness
 	 echo "2048" > /sys/block/mmcblk0/bdi/read_ahead_kb
 	 echo "2048" > /sys/block/mmcblk1/bdi/read_ahead_kb
